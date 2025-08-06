26967e31da114f28825a904d98ddcef7bf6f2439
macbook_air <2477952281@qq.com>
2025-08-06 12:12:37 +0800

    实现简易type_traits.h，替换construct.h的std::type_traits为mystl::type_traits

--- include/construct.h
+++ include/construct.h
@@ -1,15 +1,16 @@
 #ifndef MY_CONSTRUCT_H_
 #define MY_CONSTRUCT_H_
 
-#include <type_traits>
-#include <utility>
+#include <iterator>      
+#include "type_traits.h" 
+#include <utility>       
 namespace mystl {
 
 //无参数 (默认构造)
 template <typename T>
 void construct(T* ptr) {
   ::new (static_cast<void*>(ptr)) T();
-} 
+}
 
 // 单参数 (拷贝/移动构造)
 template <typename T, typename Arg>
@@ -31,7 +32,7 @@ void construct(T* ptr, Args&&... args) {
 // 为平凡析构（trivially destructible）做优化
 // 如果类型是平凡析构（std::true_type），析构函数什么都不做，直接跳过，避免没必要的析构开销。
 template <typename T>
-void destroy_one(T* /*ptr*/, std::true_type) noexcept {
+void destroy_one(T* /*ptr*/, mystl::true_type) noexcept {
   // /*ptr*/ ： 表明函数形参存在，但这个参数在函数体内没用到，所以注释掉。
   // 这样写可以避免编译器因为未使用参数而产生“未使用参数”的警告（warning）。
   // noexcept：标记函数不会抛出异常
@@ -40,7 +41,7 @@ void destroy_one(T* /*ptr*/, std::true_type) noexcept {
 
 //如果类型非平凡析构（std::false_type），才调用析构函数。
 template <typename T>
-void destroy_one(T* ptr, std::false_type) noexcept {
+void destroy_one(T* ptr, mystl::false_type) noexcept {
   if (ptr != nullptr) {
     ptr->~T();
   }
@@ -48,7 +49,31 @@ void destroy_one(T* ptr, std::false_type) noexcept {
 
 template <typename T>
 void destroy(T* ptr) noexcept {
-  destroy_one(ptr, std::is_trivially_destructible<T>{});
+  destroy_one(ptr, mystl::is_trivially_destructible<T>{});
+}
+
+template <typename ForwardIterator>
+void destroy_cat(ForwardIterator first, ForwardIterator last, mystl::true_type) {
+  // 平凡析构类型
+}
+
+template <typename ForwardIterator>
+void destroy_cat(ForwardIterator first, ForwardIterator last, mystl::false_type) {
+  for (; first != last; ++first) {
+    destroy(&*first);
+    //*first：解引用迭代器，获得当前元素引用
+    //&*first：取地址，获得元素指针
+  }
+}
+
+template <typename ForwardIterator>
+void destroy(ForwardIterator first, ForwardIterator last) {
+  destroy_cat(
+      first, last,
+      mystl::is_trivially_destructible<
+          typename std::iterator_traits<ForwardIterator>::value_type>{});
+          // value_type：提取出迭代器的类型，如std::vector<T>::iterator、int*
+          // destroy_one不需要是因为已经知道是T类型了
 }
 
 }  // namespace mystl
---
+++ include/type_traits.h
@@ -0,0 +1,31 @@
+#ifndef MY_TYPE_TRAITS_H_
+#define MY_TYPE_TRAITS_H_
+
+namespace mystl {
+template <typename T, T v>
+struct integral_constant {
+  static constexpr T value = v;
+  // constexpr:编译器就要确定值
+};
+
+// using:创建类型别名;现代化cpp应抛弃使用typedef（typedef不能处理模板相关！）
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+// 默认规则：对于任意两种类型 T 和 U，is_same<T, U> 的结果默认为 false
+template <typename T, typename U>
+struct is_same : public false_type {};
+
+template <typename T>
+struct is_same<T, T> : public true_type {};
+
+template <typename T>
+struct is_pod : public integral_constant<bool, __is_pod(T)> {};
+
+template <typename T>
+struct is_trivially_destructible
+    : public integral_constant<bool, __is_trivially_destructible(T)> {};
+
+}  // namespace mystl
+
+#endif
