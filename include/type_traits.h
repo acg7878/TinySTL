3af2ae647ff2f3119a3caa1055e0830e90613321
macbook_air <2477952281@qq.com>
2025-08-10 22:49:58 +0800

    添加注释进行讲解

--- include/type_traits.h
+++ include/type_traits.h
@@ -6,6 +6,7 @@ template <typename T, T v>
 struct integral_constant {
   static constexpr T value = v;
   // constexpr:编译器就要确定值
+  // static:直接通过类型名访问 value; 值属于类型，而非对象,没有static要先创建对象才能访问
 };
 
 // using:创建类型别名;现代化cpp应抛弃使用typedef（typedef不能处理模板相关！）
