06f53b9c75f0bf5e1bb5696c7c79b408441c0179
s_yangjunke <s_yangjunke@wps.cn>
2025-11-15 20:27:34 +0800

    docs(utility): 更新注释

--- include/mystl/__utility/move.h
+++ include/mystl/__utility/move.h
@@ -7,6 +7,8 @@ namespace mystl {
 
 template <typename T>
 // T&& arg：转发引用；涉及 引用折叠
+// 传入左值(如int x)，会被推导为int&
+// 传入右值(如42)，会被推导为int&&
 typename mystl::remove_reference<T>::type&& move(T&& arg) noexcept {
   return static_cast<typename mystl::remove_reference<T>::type&&>(arg);
 }
