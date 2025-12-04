14d10a1afef491d5a5becb3b540892eb392ad161
s_yangjunke <s_yangjunke@wps.cn>
2025-12-04 15:18:58 +0800

    refactor(vector): 移除重复的头文件引用

--- include/mystl/vector.h
+++ include/mystl/vector.h
@@ -4,7 +4,6 @@
 #include <mystl/memory.h>
 #include <mystl/utility.h>
 #include <mystl/iterator.h>
-#include <mystl/memory.h>
 #include <mystl/type_traits.h>
 #include <cstddef>
 #include <initializer_list>
